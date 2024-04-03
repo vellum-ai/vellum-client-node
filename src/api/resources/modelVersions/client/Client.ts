d4752729f856eb7f70eb2d7d976d495089bbb97d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-04-03 16:49:42 +0000

    Release 0.3.14

--- src/api/resources/modelVersions/client/Client.ts
+++ src/api/resources/modelVersions/client/Client.ts
@@ -42,7 +42,7 @@ export class ModelVersions {
                 X_API_KEY: await core.Supplier.get(this._options.apiKey),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.3.13",
+                "X-Fern-SDK-Version": "0.3.14",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
             },
