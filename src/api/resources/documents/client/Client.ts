b86b27145b4bcb5f343865d7337157b2d45946d0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-10-17 16:12:33 +0000

    SDK regeneration

--- src/api/resources/documents/client/Client.ts
+++ src/api/resources/documents/client/Client.ts
@@ -14,7 +14,7 @@ import { Blob } from "buffer";
 export declare namespace Documents {
     interface Options {
         environment?: core.Supplier<environments.VellumEnvironment | environments.VellumEnvironmentUrls>;
-        apiKey: core.Supplier<string>;
+        apiKey?: core.Supplier<string | undefined>;
     }
 
     interface RequestOptions {
@@ -28,7 +28,7 @@ export declare namespace Documents {
 }
 
 export class Documents {
-    constructor(protected readonly _options: Documents.Options) {}
+    constructor(protected readonly _options: Documents.Options = {}) {}
 
     /**
      * Used to list documents. Optionally filter on supported fields.
@@ -71,8 +71,8 @@ export class Documents {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.8.24",
-                "User-Agent": "vellum-ai/0.8.24",
+                "X-Fern-SDK-Version": "0.8.25",
+                "User-Agent": "vellum-ai/0.8.25",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -135,8 +135,8 @@ export class Documents {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.8.24",
-                "User-Agent": "vellum-ai/0.8.24",
+                "X-Fern-SDK-Version": "0.8.25",
+                "User-Agent": "vellum-ai/0.8.25",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -198,8 +198,8 @@ export class Documents {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.8.24",
-                "User-Agent": "vellum-ai/0.8.24",
+                "X-Fern-SDK-Version": "0.8.25",
+                "User-Agent": "vellum-ai/0.8.25",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -261,8 +261,8 @@ export class Documents {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.8.24",
-                "User-Agent": "vellum-ai/0.8.24",
+                "X-Fern-SDK-Version": "0.8.25",
+                "User-Agent": "vellum-ai/0.8.25",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -370,8 +370,8 @@ export class Documents {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.8.24",
-                "User-Agent": "vellum-ai/0.8.24",
+                "X-Fern-SDK-Version": "0.8.25",
+                "User-Agent": "vellum-ai/0.8.25",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/api/resources/folderEntities/client/Client.ts
+++ src/api/resources/folderEntities/client/Client.ts
@@ -5,14 +5,14 @@
 import * as environments from "../../../../environments";
 import * as core from "../../../../core";
 import * as Vellum from "../../../index";
-import * as serializers from "../../../../serialization/index";
 import urlJoin from "url-join";
+import * as serializers from "../../../../serialization/index";
 import * as errors from "../../../../errors/index";
 
 export declare namespace FolderEntities {
     interface Options {
         environment?: core.Supplier<environments.VellumEnvironment | environments.VellumEnvironmentUrls>;
-        apiKey: core.Supplier<string>;
+        apiKey?: core.Supplier<string | undefined>;
     }
 
     interface RequestOptions {
@@ -26,14 +26,108 @@ export declare namespace FolderEntities {
 }
 
 export class FolderEntities {
-    constructor(protected readonly _options: FolderEntities.Options) {}
+    constructor(protected readonly _options: FolderEntities.Options = {}) {}
+
+    /**
+     * List all folder entities within a specified folder.
+     *
+     * @param {Vellum.FolderEntitiesListRequest} request
+     * @param {FolderEntities.RequestOptions} requestOptions - Request-specific configuration.
+     *
+     * @example
+     *     await client.folderEntities.list({
+     *         parentFolderId: "parent_folder_id"
+     *     })
+     */
+    public async list(
+        request: Vellum.FolderEntitiesListRequest,
+        requestOptions?: FolderEntities.RequestOptions
+    ): Promise<Vellum.PaginatedFolderEntityList> {
+        const { entityStatus, limit, offset, ordering, parentFolderId } = request;
+        const _queryParams: Record<string, string | string[] | object | object[]> = {};
+        if (entityStatus != null) {
+            _queryParams["entity_status"] = entityStatus;
+        }
+
+        if (limit != null) {
+            _queryParams["limit"] = limit.toString();
+        }
+
+        if (offset != null) {
+            _queryParams["offset"] = offset.toString();
+        }
+
+        if (ordering != null) {
+            _queryParams["ordering"] = ordering;
+        }
+
+        _queryParams["parent_folder_id"] = parentFolderId;
+        const _response = await core.fetcher({
+            url: urlJoin(
+                ((await core.Supplier.get(this._options.environment)) ?? environments.VellumEnvironment.Production)
+                    .default,
+                "v1/folder-entities"
+            ),
+            method: "GET",
+            headers: {
+                "X-Fern-Language": "JavaScript",
+                "X-Fern-SDK-Name": "vellum-ai",
+                "X-Fern-SDK-Version": "0.8.25",
+                "User-Agent": "vellum-ai/0.8.25",
+                "X-Fern-Runtime": core.RUNTIME.type,
+                "X-Fern-Runtime-Version": core.RUNTIME.version,
+                ...(await this._getCustomAuthorizationHeaders()),
+            },
+            contentType: "application/json",
+            queryParameters: _queryParams,
+            requestType: "json",
+            timeoutMs: requestOptions?.timeoutInSeconds != null ? requestOptions.timeoutInSeconds * 1000 : undefined,
+            maxRetries: requestOptions?.maxRetries,
+            abortSignal: requestOptions?.abortSignal,
+        });
+        if (_response.ok) {
+            return serializers.PaginatedFolderEntityList.parseOrThrow(_response.body, {
+                unrecognizedObjectKeys: "passthrough",
+                allowUnrecognizedUnionMembers: true,
+                allowUnrecognizedEnumValues: true,
+                breadcrumbsPrefix: ["response"],
+            });
+        }
+
+        if (_response.error.reason === "status-code") {
+            throw new errors.VellumError({
+                statusCode: _response.error.statusCode,
+                body: _response.error.body,
+            });
+        }
+
+        switch (_response.error.reason) {
+            case "non-json":
+                throw new errors.VellumError({
+                    statusCode: _response.error.statusCode,
+                    body: _response.error.rawBody,
+                });
+            case "timeout":
+                throw new errors.VellumTimeoutError();
+            case "unknown":
+                throw new errors.VellumError({
+                    message: _response.error.errorMessage,
+                });
+        }
+    }
 
     /**
      * Add an entity to a specific folder or root directory.
      *
      * Adding an entity to a folder will remove it from any other folders it might have been a member of.
      *
-     * @param {string} folderId - The ID of the folder to which the entity should be added. This can be a UUID of a folder, or the name of a root directory (e.g. "PROMPT_SANDBOX").
+     * @param {string} folderId - The ID of the folder to which the entity should be added. This can be a UUID of a folder, or the name of a root
+     *                            directory. Supported root directories include:
+     *
+     *                            - PROMPT_SANDBOX
+     *                            - WORKFLOW_SANDBOX
+     *                            - DOCUMENT_INDEX
+     *                            - TEST_SUITE
      * @param {Vellum.AddEntityToFolderRequest} request
      * @param {FolderEntities.RequestOptions} requestOptions - Request-specific configuration.
      *
@@ -57,8 +151,8 @@ export class FolderEntities {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.8.24",
-                "User-Agent": "vellum-ai/0.8.24",
+                "X-Fern-SDK-Version": "0.8.25",
+                "User-Agent": "vellum-ai/0.8.25",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
