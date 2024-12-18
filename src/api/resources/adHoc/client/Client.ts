b0b4d8fc535cca984f5f8e4a795d5f807f58027d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-18 17:21:09 +0000

    Release 0.11.9

--- src/api/resources/adHoc/client/Client.ts
+++ src/api/resources/adHoc/client/Client.ts
@@ -46,8 +46,8 @@ export class AdHoc {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.11.8",
-                "User-Agent": "vellum-ai/0.11.8",
+                "X-Fern-SDK-Version": "0.11.9",
+                "User-Agent": "vellum-ai/0.11.9",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
