45a079312b41368d6ca54640b16a7f6dcf6b5f8c
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-18 17:11:16 +0000

    SDK regeneration

--- src/api/resources/testSuiteRuns/client/Client.ts
+++ src/api/resources/testSuiteRuns/client/Client.ts
@@ -58,8 +58,8 @@ export class TestSuiteRuns {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.11.8",
-                "User-Agent": "vellum-ai/0.11.8",
+                "X-Fern-SDK-Version": "0.11.9",
+                "User-Agent": "vellum-ai/0.11.9",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -122,8 +122,8 @@ export class TestSuiteRuns {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.11.8",
-                "User-Agent": "vellum-ai/0.11.8",
+                "X-Fern-SDK-Version": "0.11.9",
+                "User-Agent": "vellum-ai/0.11.9",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -206,8 +206,8 @@ export class TestSuiteRuns {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.11.8",
-                "User-Agent": "vellum-ai/0.11.8",
+                "X-Fern-SDK-Version": "0.11.9",
+                "User-Agent": "vellum-ai/0.11.9",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "0.11.8";
+export const SDK_VERSION = "0.11.9";
