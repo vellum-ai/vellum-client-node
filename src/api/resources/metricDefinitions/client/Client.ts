f8a0a0b69d00773a4cf4f5bbdcbf8fbc4a54d926
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-01-31 05:32:16 +0000

    SDK regeneration

--- src/api/resources/metricDefinitions/client/Client.ts
+++ src/api/resources/metricDefinitions/client/Client.ts
@@ -62,8 +62,8 @@ export class MetricDefinitions {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.13.18",
-                "User-Agent": "vellum-ai/0.13.18",
+                "X-Fern-SDK-Version": "0.13.19",
+                "User-Agent": "vellum-ai/0.13.19",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -133,8 +133,8 @@ export class MetricDefinitions {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.13.18",
-                "User-Agent": "vellum-ai/0.13.18",
+                "X-Fern-SDK-Version": "0.13.19",
+                "User-Agent": "vellum-ai/0.13.19",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
