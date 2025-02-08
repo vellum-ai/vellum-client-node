271e4eff8b3318edc1567f920883331f727a0895
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-08 21:40:54 +0000

    SDK regeneration

--- src/api/resources/workflowSandboxes/client/Client.ts
+++ src/api/resources/workflowSandboxes/client/Client.ts
@@ -58,8 +58,8 @@ export class WorkflowSandboxes {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.13.27",
-                "User-Agent": "vellum-ai/0.13.27",
+                "X-Fern-SDK-Version": "0.13.28",
+                "User-Agent": "vellum-ai/0.13.28",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
