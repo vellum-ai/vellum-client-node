84c69353efaa3b0332e72371dc415cc3f1449c8d
David Vargas <dvargas92495@gmail.com>
2025-03-07 11:35:12 -0500

    Get around node's 5min fetch timeout using undici

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,12 @@ export { VellumClient } from "./Client";
 export { VellumEnvironment, VellumEnvironmentUrls } from "./environments";
 export { VellumError, VellumTimeoutError } from "./errors";
 export * as serialization from "./serialization";
+import { setGlobalDispatcher, Agent } from "undici";
+
+setGlobalDispatcher(
+    new Agent({
+        connect: { timeout: 10_000 },
+        bodyTimeout: 0,
+        headersTimeout: 600_000,
+    })
+);
