043d21a9f5f26cac56d9d874358f92c04746cd9a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-13 23:19:40 +0000

    SDK regeneration

--- src/api/resources/metricDefinitions/client/Client.ts
+++ src/api/resources/metricDefinitions/client/Client.ts
@@ -62,8 +62,8 @@ export class MetricDefinitions {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.14.15",
-                "User-Agent": "vellum-ai/0.14.15",
+                "X-Fern-SDK-Version": "0.14.16",
+                "User-Agent": "vellum-ai/0.14.16",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -133,8 +133,8 @@ export class MetricDefinitions {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.14.15",
-                "User-Agent": "vellum-ai/0.14.15",
+                "X-Fern-SDK-Version": "0.14.16",
+                "User-Agent": "vellum-ai/0.14.16",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/serialization/resources/documentIndexes/client/requests/DocumentIndexUpdateRequest.ts
+++ src/serialization/resources/documentIndexes/client/requests/DocumentIndexUpdateRequest.ts
@@ -6,7 +6,6 @@ import * as serializers from "../../../../index";
 import * as Vellum from "../../../../../api/index";
 import * as core from "../../../../../core";
 import { EntityStatus } from "../../../../types/EntityStatus";
-import { EnvironmentEnum } from "../../../../types/EnvironmentEnum";
 
 export const DocumentIndexUpdateRequest: core.serialization.Schema<
     serializers.DocumentIndexUpdateRequest.Raw,
@@ -14,13 +13,11 @@ export const DocumentIndexUpdateRequest: core.serialization.Schema<
 > = core.serialization.object({
     label: core.serialization.string(),
     status: EntityStatus.optional(),
-    environment: EnvironmentEnum.optional(),
 });
 
 export declare namespace DocumentIndexUpdateRequest {
     export interface Raw {
         label: string;
         status?: EntityStatus.Raw | null;
-        environment?: EnvironmentEnum.Raw | null;
     }
 }
