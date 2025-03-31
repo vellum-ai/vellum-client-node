ff95120427c7a138209c279e97daacbb9ab84e24
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-31 22:20:40 +0000

    SDK regeneration

--- src/api/resources/organizations/client/Client.ts
+++ src/api/resources/organizations/client/Client.ts
@@ -52,8 +52,8 @@ export class Organizations {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.14.26",
-                "User-Agent": "vellum-ai/0.14.26",
+                "X-Fern-SDK-Version": "0.14.27",
+                "User-Agent": "vellum-ai/0.14.27",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
