8c4ccd4d4ffccaba874b0cce5ce5dab6d2f0cc22
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-09 04:11:06 +0000

    SDK regeneration

--- src/api/resources/workflows/client/Client.ts
+++ src/api/resources/workflows/client/Client.ts
@@ -81,8 +81,8 @@ export class Workflows {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.14.55",
-                "User-Agent": "vellum-ai/0.14.55",
+                "X-Fern-SDK-Version": "0.14.56",
+                "User-Agent": "vellum-ai/0.14.56",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -197,8 +197,8 @@ export class Workflows {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.14.55",
-                "User-Agent": "vellum-ai/0.14.55",
+                "X-Fern-SDK-Version": "0.14.56",
+                "User-Agent": "vellum-ai/0.14.56",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "0.14.55";
+export const SDK_VERSION = "0.14.56";
