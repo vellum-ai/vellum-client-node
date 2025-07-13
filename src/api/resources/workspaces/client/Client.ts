2eb483a2a4a29cbbbf7a25bd7ea388f04514fe2a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-13 05:01:34 +0000

    Release 0.14.88

--- src/api/resources/workspaces/client/Client.ts
+++ src/api/resources/workspaces/client/Client.ts
@@ -60,8 +60,8 @@ export class Workspaces {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "0.14.87",
-                "User-Agent": "vellum-ai/0.14.87",
+                "X-Fern-SDK-Version": "0.14.88",
+                "User-Agent": "vellum-ai/0.14.88",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
