25aaee5f03bdb94fd3bfd9a0a67646b7985cb4a1
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-25 16:31:35 +0000

    SDK regeneration

--- src/serialization/types/OrganizationRead.ts
+++ src/serialization/types/OrganizationRead.ts
@@ -6,7 +6,6 @@ import * as serializers from "../index";
 import * as Vellum from "../../api/index";
 import * as core from "../../core";
 import { NewMemberJoinBehaviorEnum } from "./NewMemberJoinBehaviorEnum";
-import { OrganizationLimitConfig } from "./OrganizationLimitConfig";
 
 export const OrganizationRead: core.serialization.ObjectSchema<
     serializers.OrganizationRead.Raw,
@@ -16,7 +15,10 @@ export const OrganizationRead: core.serialization.ObjectSchema<
     name: core.serialization.string(),
     allowStaffAccess: core.serialization.property("allow_staff_access", core.serialization.boolean().optional()),
     newMemberJoinBehavior: core.serialization.property("new_member_join_behavior", NewMemberJoinBehaviorEnum),
-    limitConfig: core.serialization.property("limit_config", OrganizationLimitConfig),
+    limitConfig: core.serialization.property(
+        "limit_config",
+        core.serialization.record(core.serialization.string(), core.serialization.unknown()).optional(),
+    ),
 });
 
 export declare namespace OrganizationRead {
@@ -25,6 +27,6 @@ export declare namespace OrganizationRead {
         name: string;
         allow_staff_access?: boolean | null;
         new_member_join_behavior: NewMemberJoinBehaviorEnum.Raw;
-        limit_config: OrganizationLimitConfig.Raw;
+        limit_config?: Record<string, unknown> | null;
     }
 }
