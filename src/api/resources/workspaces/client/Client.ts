20b88e3b3b4d944fc02d6658c3986644b64f2f48
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-23 21:28:05 +0000

    SDK regeneration

--- src/api/resources/workspaces/client/Client.ts
+++ src/api/resources/workspaces/client/Client.ts
@@ -67,11 +67,11 @@ export class Workspaces {
                         ? serializers.ApiVersionEnum.jsonOrThrow(await core.Supplier.get(this._options.apiVersion), {
                               unrecognizedObjectKeys: "strip",
                           })
-                        : "2025-07-30",
+                        : undefined,
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "1.5.0",
-                "User-Agent": "vellum-ai/1.5.0",
+                "X-Fern-SDK-Version": "1.5.1",
+                "User-Agent": "vellum-ai/1.5.1",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "1.5.0";
+export const SDK_VERSION = "1.5.1";
