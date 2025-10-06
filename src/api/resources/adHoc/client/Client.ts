a75eb792d1a9053b29cab926330277f2e03ff262
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-10-06 20:45:50 +0000

    SDK regeneration

--- src/api/resources/adHoc/client/Client.ts
+++ src/api/resources/adHoc/client/Client.ts
@@ -101,11 +101,11 @@ export class AdHoc {
                         ? serializers.ApiVersionEnum.jsonOrThrow(await core.Supplier.get(this._options.apiVersion), {
                               unrecognizedObjectKeys: "strip",
                           })
-                        : "2025-07-30",
+                        : undefined,
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "1.7.1",
-                "User-Agent": "vellum-ai/1.7.1",
+                "X-Fern-SDK-Version": "1.7.2",
+                "User-Agent": "vellum-ai/1.7.2",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -189,11 +189,11 @@ export class AdHoc {
                         ? serializers.ApiVersionEnum.jsonOrThrow(await core.Supplier.get(this._options.apiVersion), {
                               unrecognizedObjectKeys: "strip",
                           })
-                        : "2025-07-30",
+                        : undefined,
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "vellum-ai",
-                "X-Fern-SDK-Version": "1.7.1",
-                "User-Agent": "vellum-ai/1.7.1",
+                "X-Fern-SDK-Version": "1.7.2",
+                "User-Agent": "vellum-ai/1.7.2",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
